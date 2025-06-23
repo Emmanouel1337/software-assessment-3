602fc8e8d5a9a19980c2d666b53cb2549f6e4b58
Emmanouel1337 <emmanouel.voudouris@rbslearn.net>
2025-06-23 02:21:43 +0000

    added a getBeaten function, sorting by the greatest negative values for hours_leftsp and hours_leftmp to find the games which have probably been beaten.

--- checkfull.py
+++ checkfull.py
@@ -11,9 +11,17 @@ def getAsc(steamid):
     return results
 
 def getDesc(steamid):
-    sqliteConnection2 = sqlite3.connect(f'/workspaces/software-assessment-3/{steamid}_fulldatabase.db')
-    cursor2 = sqliteConnection2.cursor()
+    conn = sqlite3.connect(f'/workspaces/software-assessment-3/{steamid}_fulldatabase.db')
+    cursor2 = conn.cursor()
     cursor2.execute(f"""SELECT *, CASE WHEN complexity_1v1_sp = 1 AND hours_leftsp >= 0 THEN hours_leftsp WHEN complexity_1v1_mp = 1 AND hours_leftmp >= 0 THEN hours_leftmp ELSE NULL END AS min_time_left FROM "{steamid}_fulldatabase" WHERE (complexity_1v1_sp = 1 AND hours_leftsp >= 0) OR (complexity_1v1_mp = 1 AND hours_leftmp >= 0) ORDER BY min_time_left DESC NULLS LAST LIMIT 8;""")
+    cursor2.close()
+    conn.close()
     return cursor2.fetchall()
 
-
+def getBeaten(steamid):
+    conn = sqlite3.connect(f'/workspaces/software-assessment-3/{steamid}_fulldatabase.db')
+    cursor = conn.cursor()
+    cursor.execute(f"""SELECT game, hours_leftsp, hours_leftmp FROM "{steamid}_fulldatabase" WHERE (hours_leftsp <= 0 OR hours_leftmp <= 0) ORDER BY GREATEST(hours_leftsp, hours_leftmp) ASC LIMIT 8;""")
+    cursor.close()
+    conn.close()
+    return cursor.fetchall
